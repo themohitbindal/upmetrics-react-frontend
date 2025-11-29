a5dd4fbfbee700a3ad05c4dd8c44c2b6b402974a
themohitbindal <themohitbindal@gmail.com>
2025-11-29 16:38:44 +0530

    initial react basic setup commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Upmetrics React Frontend</h1>
+      <p>Welcome to your React application</p>
+    </div>
+  )
+}
+
+export default App
